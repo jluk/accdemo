6a3e356dfcbf31604cb94bab49849a10d7ae7b3d
jluk <juluk@microsoft.com>
2017-01-20 13:11:46 -0800

    adding messages

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
+import os
+import platform
 from urllib2 import urlopen
 my_ip = urlopen('http://ip.42.pl/raw').read()
-#print(my_ip)
+print("Your public IP address is " + my_ip + ".")
+print("Your OS is " + os.name + ".")
+print("Your system is " + platform.system() + " version " + platform.release() + ".")
